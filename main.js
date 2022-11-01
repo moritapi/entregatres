b422940a87c6e81294bf20b31f3e2a83005cc3e6
moritapi <moritapi@gmail.com>
2022-11-01 14:46:38 -0500

    añadí localstorage, json a happypet

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
-let track = document.getElementById("track");
+let taskData = [];
+let i = null;
+function Submit() {
+    if (taskData.length > 0) {
+        let mascota = document.getElementById('tarea').value;
+        let nombretenedor = document.getElementById('nombretenedor').value;
+        let edad = document.getElementById('edad').value;
+        let raza = document.getElementById('raza').value;
+        const obj = { "mascota": mascota, "nombretenedor": nombretenedor, "edad": edad, "raza": raza, }
+        let stored = JSON.parse(localStorage.getItem("task"));
+        stored.push(obj);
+    }
+    let mascota = document.getElementById('mascota').value;
+    let nombretenedor = document.getElementById('nombretenedor').value;
+    let edad = document.getElementById('edad').value;
+    let raza = document.getElementById('raza').value;
+    const obj = { "mascota": mascota, "nombretenedor": nombretenedor, "edad": edad, "raza": raza, }
+    taskData.push(obj);
 
-let controlBtn = document.getElementById("play-pause");
+    localStorage.setItem("task", JSON.stringify(taskData));
+    document.getElementById("result").innerHTML = localStorage.getItem("task");
 
-function playPause() {
-    if (track.paused) {
-        track.play();
+    if (i == null) {
+        i = 0;
+    }
 
-        controlBtn.className = "pause";
-    } else { 
-        track.pause();
+    for (i = i; i < taskData.length; ++i) {
 
-        controlBtn.className = "play";
+        let rowCount = table.rows.length;
+        let row = table.insertRow(rowCount);
+        row.insertCell(0).innerHTML = i + 1;
+        row.insertCell(1).innerHTML = taskData[i].mascota;
+        row.insertCell(2).innerHTML = taskData[i].nombretenedor;
+        row.insertCell(4).innerHTML = taskData[i].edad;
+        row.insertCell(5).innerHTML = taskData[i].raza;
+        i = i;
     }
 }
-controlBtn.addEventListener("click", playPause);
-track.addEventListener("ended", function() {
-  controlBtn.className = "play";
-});
